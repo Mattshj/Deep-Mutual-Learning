d9a7d44df973ac8528ca3f8541440a672e9324d2
feknall <h.fazli.k@gmail.com>
2022-04-29 19:31:34 -0400

    add different runners

---
+++ train_image_classifier_garbage_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=def-dhadidi
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8000M               # memory (per node)
+#SBATCH --time=0-1:00            # time (DD-HH:MM)
+#SBATCH --mail-user=h.fazli.k@gmail.com
+#SBATCH --mail-type=ALL
+python train_image_classifier_garbage_2.py
